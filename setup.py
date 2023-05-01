01eed6a0d9c192c45238f161616e6e41897af394
Mallesh <hmallesh04@gmail.com>
2023-05-01 16:15:57 +0530

    all file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="cencus-income",
+      version="0.0.1",
+      author="mallesh",
+      author_mail="hmallesh04@gmail.com",
+      packages=find_packages(),
+      install_requires=["pandas","numpy","flask"]
+      )
